54962131fa72dca6edc6a2515103929386f7a5d0
raine2703 <raine8723@gmial.com>
2023-02-23 07:01:31 +0000

    Initial

---
+++ code.ps1
@@ -0,0 +1,47 @@
+#Basics
+
+$PSVersionTable
+
+Get-InstalledModule
+
+Get-ComputerInfo
+
+(Get-ComputerInfo).OsInstallDate
+
+Get-Service -Name "d*"
+
+Get-Service -Displayname "*network*"
+
+#Variables
+$x = 5
+$y = 6
+$z=$x+$y
+$z
+$CourseName="Azure PowerShell"
+"Value of x is $x"
+'The value of x is ' + $x + ' Text continues'
+$x.GetType()
+$CourseName.GetType()
+
+#Arrays
+$coursevideos = 'abc','def','ghj'
+$coursevideos
+
+$coursenumbers = 123,456,789
+$coursenumbers
+
+#Arrays
+$coursevideos_1=@(
+    'abc'
+    'cad'
+    'asdx'
+)
+$coursevideos_1
+$coursevideos_1[0]
+
+#Updating array value
+$coursevideos_1[0] = 'changed'
+$coursevideos_1
+
+#Key vaulue stores
+#Hash tables

238ee24ff2e5ed523aae547630c9b4972c0503fc
raine2703 <raine2703@gmial.com>
2023-03-09 19:24:34 +0000

    last

--- Basics/statements.ps1
+++ Basics/statements.ps1
@@ -10,3 +10,49 @@ if($NumberOfVideos -ge 20) {
 }
 
 #While statement
+
+$i=1
+while ($i -le 10) {
+    $i
+    ++$i
+ }
+
+ 
+#for statement
+
+for($i=1; $i -le 10; ++$i){
+    $i
+}
+
+#for each
+$coursevideos = 'abc','def','ghj'
+
+foreach ($x in $coursevideos) {
+    $x + ' text'
+}
+
+
+
+$CourseList=@(
+    [PSCustomObject]@{
+        ID = 1
+        Rating = 777
+        Name = 'Azure Admin'
+    },
+    [PSCustomObject]@{
+        ID = 2
+        Rating = 999
+        Name = 'Azure Dev'
+    },
+    [PSCustomObject]@{
+        ID = 3
+        Rating = 888
+        Name = 'Azure Sec'
+    }
+)
+
+foreach ($x in $CourseList) {
+    $x.ID
+    $x.Rating
+    $x.Name
+}

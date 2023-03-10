4f02aa42646f2302ecc5ac15eaeab15d77d62878
raine2703 <raine2703@gmial.com>
2023-03-10 16:03:10 +0000

    ok

--- Basics/objects.ps1
+++ Basics/objects.ps1
@@ -34,3 +34,58 @@ $CourseList[0].name
 
 
 #Continuing working with Objects
+
+$Mobiles=@(
+    [PSCustomObject]@{
+        Brand = "Samsung"
+        Model = "S22"
+        Storage=@("128","256","512")
+        DefaultApps=@(
+            @{
+                Name="Google Maps"
+                Status="Installed"
+            },
+            @{
+                Name="FireFox"
+                Stats="Disabled"
+            }
+        )
+    },
+    [PSCustomObject]@{
+        Brand = "Samsung"
+        Model = "S23"
+        Storage=@("8","16","32")
+        DefaultApps=@(
+            @{
+                Name="Google Maps"
+                Status="Installed"
+            },
+            @{
+                Name="FireFox"
+                Status="Disabled"
+            }
+        )
+    }
+)
+$Mobiles
+$mobiles[0]
+$Mobiles[0].Storage[1]
+$Mobiles[0].DefaultApps[0]
+$Mobiles[0].DefaultApps.Item(0)
+$mobiles.DefaultApps
+
+foreach($x in $Mobiles[0]) {
+    $x.Brand
+    $x.Model
+    $x.Storage
+    $x.DefaultApps
+}
+
+foreach($y in $Mobiles.DefaultApps) {
+    $y.name + ' is ' + $y.Status
+    }
+
+$Mobiles | Where-Object {$_.Model -eq "S23"}
+
+$Mobiles | Where-Object {$_.Model -eq "S23"} | Select-Object -Property Model,Storage
+$Mobiles | Where-Object {$_.Model -eq "S23"} | Select-Object -Property DefaultApps

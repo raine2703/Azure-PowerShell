0dc8b3d488da9802fafe1e38b7e69e1be69109ff
raine2703 <raine2703@gmial.com>
2023-03-21 08:07:01 +0000

    ok

--- Basics/functions.ps1
+++ Basics/functions.ps1
@@ -1,6 +1,4 @@
 
-
-
 function get-appversionx {
     $PSVersionTable
 }
@@ -11,3 +9,37 @@ function sumofnumbers([int]$x, [int]$y) {
 sumofnumbers 5 9
 
 
+function Get-Course
+{
+    param(
+        [Object[]] $CourseList2x
+    )
+
+    foreach($x in $CourseList2x)
+{
+    $x.Id
+    $x.Name
+    $x.Rating
+}
+}
+
+$CourseList=@(
+    [PSCustomObject]@{
+        Id = 1
+        Name ='AZ-104 Azure Administrator'
+        Rating = 4.7
+    },
+    [PSCustomObject]@{
+        Id = 2
+        Name ='AZ-305 Azure Architect Design'
+        Rating = 4.8
+    },
+    [PSCustomObject]@{
+        Id = 3
+        Name ='AZ-500 Azure Security'
+        Rating = 4.9
+    }
+)
+
+
+Get-Course $CourseList

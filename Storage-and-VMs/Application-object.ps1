eacd27d79c01445ff3e1da64bc5df6c5d7cef63a
raine2703 <raine2703@gmial.com>
2023-03-21 12:16:29 +0000

    update

---
+++ Storage-and-VMs/Application-object.ps1
@@ -0,0 +1,21 @@
+#Connecting to Azure subscription using Application ID.
+#Correct way to manage resources.
+#Application is registered in Azure AD. Permissions to it are granted via RBAC.
+
+$Appid = "2825a820-aceb-4cc5-a33b-888cd0c9a7e2"
+$Appsecret = "g-P8Q~iQodLcTzDc_K3LL~VxUwPIM.PgP1Zi6bzS"
+$Securesecret = $Appsecret | ConvertTo-SecureString -AsPlainText -force
+$Credential = New-Object -TypeName System.Management.Automation.PSCredential `
+-ArgumentList $Appid, $Securesecret
+$TenandID = "c20a49b8-2914-4624-a197-1e882bd3abf4"
+
+Connect-AzAccount -ServicePrincipal -Credential $Credential -Tenant $TenandID
+
+$ResourceGroupName ="powershell-grp"
+$Location = "North Europe"
+
+New-AzResourceGroup -Name $ResourceGroupName -Location $Location
+
+Get-AzResourceGroup | format-table
+
+Remove-AzResourceGroup -name $ResourceGroupName

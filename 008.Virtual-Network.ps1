aa7e15648bc10ae40e8f72c505d71b7e93b3d5af
raine2703 <raine2703@gmial.com>
2023-03-23 10:05:56 +0000

    .

---
+++ 008.Virtual-Network.ps1
@@ -0,0 +1,37 @@
+#Creating and deleting Vnet with Subnet
+
+$ResourceGroupName ="RG3"
+$Location = "North Europe"
+
+$VirtualNetworkName="Vnet"
+$VirtualNetworkAddressSpace="10.0.0.0/16"
+$SubnetName="SubnetA"
+$SubnetAddressSpace="10.0.0.0/24"
+
+#Default Subnet config
+$Subnet=New-AzVirtualNetworkSubnetConfig -Name $SubnetName -AddressPrefix $SubnetAddressSpace
+
+#Creating Vnet
+$VirtualNetwork = New-AzVirtualNetwork -Name $VirtualNetworkName -ResourceGroupName $ResourceGroupName `
+-Location $Location -AddressPrefix $VirtualNetworkAddressSpace -Subnet $Subnet
+
+#Checking result
+$VirtualNetwork
+(Get-AzVirtualNetwork).name
+
+#Adding another subnet
+Add-AzVirtualNetworkSubnetConfig -Name "SubnetC" -AddressPrefix "10.0.2.0/24" -VirtualNetwork $virtualNetwork
+#Applying configuration
+$virtualNetwork | Set-AzVirtualNetwork
+
+$virtualNetwork.subnets | format-table
+
+#Removing subnet
+Remove-AzVirtualNetworkSubnetConfig -Name "SubnetC" ` -VirtualNetwork $virtualNetwork
+$virtualNetwork | Set-AzVirtualNetwork
+$virtualNetwork.subnets | format-table
+
+#Delete Vnet
+Remove-AzVirtualNetwork -Name $VirtualNetworkName -ResourceGroupName $ResourceGroupName -Force
+(Get-AzVirtualNetwork).name
+

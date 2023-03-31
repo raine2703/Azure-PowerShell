4c8decd8e0a9c789a610503964115819aec262dc
raine2703 <raine2703@gmial.com>
2023-03-31 02:32:53 +0000

    .

--- Storage-and-VMs/020.Multiple-VMs.ps1
+++ Storage-and-VMs/020.Multiple-VMs.ps1
@@ -128,16 +128,16 @@ $Credential = New-Object System.Management.Automation.PSCredential ($UserName, $
 $VMs=@()
 for($i=1;$i -le 2;$i++)
 {
-$VirtualMachine=New-AzVMConfig -VMName $VMName$i -VMSize $VMSize -AvailabilitySetId $AvailabilitySet.Id
-$VirtualMachine=Set-AzVMOperatingSystem -VM $VirtualMachine -Windows -ComputerName $VmName$i -Credential $Credential
-$VirtualMachine=Add-AzVMNetworkInterface -VM $VirtualMachine -Id $NetworkInterfaces[$i-1].Id
-$VirtualMachine=Set-AzVMSourceImage -VM $VirtualMachine -PublisherName 'MicrosoftWindowsServer' -Offer 'WindowsServer' -Skus '2019-Datacenter' -Version latest
-$VirtualMachine=Set-AzVMBootDiagnostic -Disable -VM $VirtualMachine
+    $VirtualMachine=New-AzVMConfig -VMName $VMName$i -VMSize $VMSize -AvailabilitySetId $AvailabilitySet.Id
+    $VirtualMachine=Set-AzVMOperatingSystem -VM $VirtualMachine -Windows -ComputerName $VmName$i -Credential $Credential
+    $VirtualMachine=Add-AzVMNetworkInterface -VM $VirtualMachine -Id $NetworkInterfaces[$i-1].Id
+    $VirtualMachine=Set-AzVMSourceImage -VM $VirtualMachine -PublisherName 'MicrosoftWindowsServer' -Offer 'WindowsServer' -Skus '2019-Datacenter' -Version latest
+    $VirtualMachine=Set-AzVMBootDiagnostic -Disable -VM $VirtualMachine
 
-#Saving VM names in to array for later use
-$VMs+="$VMName$i"
+    #Saving VM names in to array for later use
+    $VMs+="$VMName$i"
 
-New-AzVM -ResourceGroupName $ResourceGroupName -Location $Location -VM $VirtualMachine
+    New-AzVM -ResourceGroupName $ResourceGroupName -Location $Location -VM $VirtualMachine
 
 }
 

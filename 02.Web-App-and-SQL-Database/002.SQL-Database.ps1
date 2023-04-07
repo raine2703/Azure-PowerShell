0f8168e0fe9a9244ae0be6675d287b3168726ff4
raine2703 <raine2703@gmial.com>
2023-04-07 20:33:49 +0000

    .

---
+++ 02.Web-App-and-SQL-Database/002.SQL-Database.ps1
@@ -0,0 +1,51 @@
+#Creating Azure SQL Server
+
+
+$ResourceGroupName="RG7"
+$Location="North Europe"
+#New-Guid generates random nuber. Converting it to String and appending first 6 characters for unique Servername
+$ServerName="dbserver" + (New-Guid).ToString().Substring(1,6)
+$AdminUser="rn2703"
+$AdminPassword="anSiubij2@1434s"
+
+
+#Credentials for Server
+$PasswordSecure=ConvertTo-SecureString -String $AdminPassword -AsPlainText -Force
+$Credential = New-Object -TypeName System.Management.Automation.PSCredential `
+-ArgumentList $AdminUser,$PasswordSecure
+
+
+#Creating SQL Server
+New-AzResourceGroup -Name $ResourceGroupName -Location $Location
+New-AzSQLServer -ResourceGroupName $ResourceGroupName -ServerName $ServerName `
+-Location $Location -SqlAdministratorCredentials $Credential
+
+
+#Deploying Database
+$DatabaseName="db01"
+New-AzSqlDatabase -ResourceGroupName $ResourceGroupName -DatabaseName $DatabaseName `
+-RequestedServiceObjectiveName "S0" -ServerName $ServerName
+
+
+#Allowing connection from my IP
+$IPAddress=Invoke-WebRequest -uri "https://ifconfig.me/ip" | Select-Object Content
+
+New-AzSqlServerFirewallRule -ResourceGroupName $ResourceGroupName `
+-ServerName $ServerName -FirewallRuleName "Allow-Client" `
+-StartIpAddress $IPAddress.Content -EndIpAddress $IPAddress.Content
+
+
+#Different DB Pricing tiers available in region
+Get-AzSqlServerServiceObjective -Location $Location
+
+
+
+#Seeding SQL Database
+$ScriptFile="C:\Users\raitisn\Desktop\Azure-PowerShell\02.Web-App-and-SQL-Database\data.sql"
+Get-AzSqlServer -ResourceGroupName $ResourceGroupName | format-table
+
+#Install-Module -Name SqlServer
+
+#Running SQL comands to Azure SQL DB
+Invoke-SqlCmd -ServerInstance "dbserver5f9f51.database.windows.net" -Database "db01" `
+-Username $AdminUser -Password $AdminPassword -InputFile $ScriptFile

a855f411d2fa0459463a37ea6eddfdf1d630e886
raine2703 <118910194+raine2703@users.noreply.github.com>
2023-05-23 00:48:10 +0300

    Update 001.Connecting-To-Azure.ps1

--- 01.Storage-and-VMs/001.Connecting-To-Azure.ps1
+++ 01.Storage-and-VMs/001.Connecting-To-Azure.ps1
@@ -2,16 +2,16 @@
 #Correct way to manage resources.
 #Application is registered in Azure AD. Permissions to it are granted via RBAC.
 
-$Appid="2825a820-aceb-4cc5-a33b-888cd0c9a7e2"
-$Appsecret="g-P8Q~iQodLcTzDc_K3LL~VxUwPIM.PgP1Zi6bzS"
+$Appid=""
+$Appsecret=""
 $Securesecret=$Appsecret | ConvertTo-SecureString -AsPlainText -force
 $Credential=New-Object -TypeName System.Management.Automation.PSCredential `
 -ArgumentList $Appid, $Securesecret
-$TenandID="c20a49b8-2914-4624-a197-1e882bd3abf4"
+$TenandID=""
 
 Connect-AzAccount -ServicePrincipal -Credential $Credential -Tenant $TenandID
 
-$SubscriptionName="Pay-As-You-Go"
+$SubscriptionName=""
 $Subcription=Get-AzSubscription -SubscriptionName $SubscriptionName
 
 #Set Subscription to work with
